1104a43e834fc5770dad395fd79b4e3e9e14fbc9
Nathan Gaul <nathanjgaul@gmail.com>
2025-04-22 20:24:18 -0500

    fix Contact component and integrate theme-change for improved theming

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import '../app.css';
+	import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
 
 	let { children } = $props();
+
+	onMount(() => {
+		themeChange(false);
+	})
 </script>
 
 {@render children()}
