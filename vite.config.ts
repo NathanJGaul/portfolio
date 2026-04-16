f2f965d32f09df5a27d39a339a3b68b763eb4dc6
Nathan Gaul <nathanjgaul@gmail.com>
2026-04-16 17:51:17 -0500

    Add wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [tailwindcss(), sveltekit(), cloudflare()],
 	server: {
 		allowedHosts: ['laptop.alewife-deneb.ts.net']
 	}
