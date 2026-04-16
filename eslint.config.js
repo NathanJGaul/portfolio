92e0e30bbd4f89c33e62c5e1d5cab7d2bb230086
Nathan Gaul <nathanjgaul@gmail.com>
2026-04-16 14:59:01 -0500

    Fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,25 @@
-import js from '@eslint/js';
-import svelte from 'eslint-plugin-svelte';
-import globals from 'globals';
-import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config.js';
-
 export default ts.config(
-	{
-		ignores: ['**/.git/**', '**/.wrangler/**', '**/node_modules/**', '**/.svelte-kit/**']
-	},
-	js.configs.recommended,
-	...ts.configs.recommended,
-	...svelte.configs.recommended,
-	{
-		languageOptions: {
-			globals: { ...globals.browser, ...globals.node }
-		},
-		rules: { 'no-undef': 'off' }
-	},
-	{
-		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
-		languageOptions: {
-			parserOptions: {
-				projectService: true,
-				extraFileExtensions: ['.svelte'],
-				parser: ts.parser,
-				svelteConfig
-			}
-		}
-	}
+  {
+    ignores: ['**/.git/**', '**/.wrangler/**', '**/node_modules/**', '**/.svelte-kit/**']
+  },
+  js.configs.recommended,
+  ts.configs.recommended,
+  svelte.configs.recommended,
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node }
+    },
+    rules: { 'no-undef': 'off' }
+  },
+  {
+    files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: ['.svelte'],
+        parser: ts.parser,
+        svelteConfig
+      }
+    }
+  }
 );
