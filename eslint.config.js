ba4ffb3137b9daeaea327c79aa3643ddd8ec385b
Nathan Gaul <nathanjgaul@gmail.com>
2026-04-16 10:37:14 -0500

    Fix cloudflare build

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,16 @@
-import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
-import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
-import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
 
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
-
 export default ts.config(
-	includeIgnoreFile(gitignorePath),
+	{
+		ignores: ['**/.git/**', '**/.wrangler/**', '**/node_modules/**', '**/.svelte-kit/**']
+	},
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs.recommended,
-	prettier,
-	...svelte.configs.prettier,
 	{
 		languageOptions: {
 			globals: { ...globals.browser, ...globals.node }
